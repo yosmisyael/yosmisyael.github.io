dd6b9470f83b78d621003562f325ec8180875c31
yosmisyael <yosmisyael7308@gmail.com>
2025-07-06 05:44:10 +0700

    feat: add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Intro from '$lib/components/Intro.svelte';
+	import Education from '$lib/components/Education.svelte';
+</script>
+
+<Intro />
+<Education />
---
+++ src/routes/about/+page.ts
@@ -0,0 +1,8 @@
+import type { SEO } from '$lib/types/seo';
+
+export function load() {
+	return {
+		title: 'About',
+		description: 'Misyael Yosevian is a software engineer and backend developer from Indonesia.'
+	} as SEO;
+}
