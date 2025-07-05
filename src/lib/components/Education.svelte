8ff9c7717e5de7053b08411a959da6ed1f5bb278
yosmisyael <yosmisyael7308@gmail.com>
2025-07-06 05:43:15 +0700

    feat: add components for about page

---
+++ src/lib/components/Education.svelte
@@ -0,0 +1,38 @@
+<script>
+	import CirclePattern from '$lib/core/icons/CirclePattern.svelte';
+	import Meet from '$lib/core/icons/Meet.svelte';
+	import Education from '$lib/core/icons/Education.svelte';
+</script>
+
+<div class="text-primary-dark flex flex-col lg:flex-row items-center justify-between px-4 md:px-10 lg:px-20 xl:px-52 pt-30 gap-5 lg:gap-32 pb-10">
+	<div class="flex flex-col lg:w-3/4 lg:order-2">
+		<div class="inline-flex items-center gap-3 mb-8">
+			<Education />
+			<h1 class="section-header-icon" data-text="Education">Education</h1>
+		</div>
+		<p class="section-description mb-5">
+			I am currently pursuing my degree in Informatics Engineering at the Electronic Engineering
+			Polytechnic Institute of Surabaya (PENS/EEPIS), where I've gained a strong theoretical
+			foundation in computer science, from algorithms to software engineering principles.
+		</p>
+		<p class="section-description mb-5">
+			Beyond the classroom, I actively sharpen my practical skills and leadership abilities as a
+			Core Team member of the Google Developer Student Clubs (GDSC), leading the Cloud Computing
+			division. This role, along with my active participation in software development communities,
+			allows me to collaborate, mentor, and stay current with industry trends.
+		</p>
+		<p class="section-description">
+			To ensure my skills are always job-ready, I am committed to self-study on platforms like
+			Dicoding, Google Cloud Skills Boost, AWS Academy, and CodePolitan, completing specialized
+			learning paths in backend development, cloud architecture, and more.
+		</p>
+	</div>
+	<div class="relative flex items-baseline lg:items-start justify-center lg:order-1">
+		<div class="relative w-full sm:px-20 md:px-40 lg:px-0">
+			<CirclePattern />
+		</div>
+		<div class="absolute z-10 w-full sm:px-20 md:px-40 lg:px-0">
+			<Meet />
+		</div>
+	</div>
+</div>
---
+++ src/lib/components/Intro.svelte
@@ -0,0 +1,40 @@
+<script>
+	import CirclePattern from '$lib/core/icons/CirclePattern.svelte';
+	import Self from '$lib/core/icons/Self.svelte';
+	import Story from '$lib/core/icons/Story.svelte';
+</script>
+
+<div class="text-primary-dark flex flex-col lg:flex-row items-center justify-between px-4 md:px-10 lg:px-20 xl:px-52 pt-30 gap-5">
+	<div class="flex flex-col">
+		<div class="inline-flex items-center gap-3 mb-8">
+			<Story />
+			<h1 class="section-header-icon" data-text="Quick Intro">Quick Intro</h1>
+		</div>
+		<p class="section-description xl:w-3/4">
+			From a 2020 lockdown curiosity to a full-stack engineer, my five-year journey has been one of
+			rapid, hands-on learning and a relentless drive to build effective solutions.
+		</p>
+		<ul class="list-disc pl-6 text-lg xl:w-3/4">
+			<li>
+				Full-Stack Web Development: Architecting, building, and deploying robust web applications
+				designed for performance and scale.
+			</li>
+			<li>
+				Intelligent Feature Integration: Leveraging foundational skills in Machine Learning and IoT
+				to add data-driven insights and smart connectivity to projects.
+			</li>
+			<li>
+				Cloud-Native Deployment: Proficient in cloud fundamentals on AWS and GCP, ensuring every
+				project I build is ready for a real-world, scalable deployment from day one.
+			</li>
+		</ul>
+	</div>
+	<div class="relative flex items-baseline xl:items-end justify-center">
+		<div class="absolute w-full sm:px-20 md:px-40 lg:px-0">
+			<CirclePattern />
+		</div>
+		<div class="relative z-10 w-full sm:px-20 md:px-40 lg:px-0">
+			<Self />
+		</div>
+	</div>
+</div>
