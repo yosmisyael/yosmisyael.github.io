b62eb67889325ee63d5a7777a386b5e75b8d11fc
yosmisyael <yosmisyael7308@gmail.com>
2025-07-07 20:24:57 +0700

    feat: adjust z index of footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="relative">
-	<p class="md:fixed text-center md:text-left pb-2 pt:10 md:pb-0 md:pt-0 md:origin-bottom-left md:rotate-90 z-50 md:left-5 md:pr-8 md:top-7/12 text-primary-dark">
+	<p class="md:fixed text-center md:text-left pb-2 pt:10 md:pb-0 md:pt-0 md:origin-bottom-left md:rotate-90 z-40 md:left-5 md:pr-8 md:top-7/12 text-primary-dark">
 		© MMXXV. 東ジャワで開発されました。
 	</p>
 </footer>
