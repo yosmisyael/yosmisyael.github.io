5bf5d4a079efe5b85aa89b765657f95a497957a3
yosmisyael <yosmisyael7308@gmail.com>
2025-07-11 01:08:54 +0700

    feat: add error fallback

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<section
+	class="flex min-h-screen flex-col items-center overflow-hidden select-none justify-center text-primary-dark"
+>
+	<h1 class="font-raleway text-8xl uppercase font-bold">404</h1>
+	<h2 class="uppercase font-sans text-3xl font-bold">Not Found</h2>
+</section>
