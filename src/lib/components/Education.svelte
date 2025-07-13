4a9a4913a49a25a406efadeff6fd8de3a2955332
yosmisyael <yosmisyael7308@gmail.com>
2025-07-13 14:09:55 +0700

    feat: add animation at about page

--- src/lib/components/Education.svelte
+++ src/lib/components/Education.svelte
@@ -1,38 +1,102 @@
-<script>
+<script lang="ts">
 	import CirclePattern from '$lib/core/icons/CirclePattern.svelte';
 	import Meet from '$lib/core/icons/Meet.svelte';
 	import Education from '$lib/core/icons/Education.svelte';
+	import { Motion } from 'svelte-motion';
+
+	const contents: string[] = [
+		"I am currently pursuing my degree in Informatics Engineering at the Electronic Engineering\n" +
+		"\t\t\t\tPolytechnic Institute of Surabaya (PENS/EEPIS), where I've gained a strong theoretical\n" +
+		"\t\t\t\tfoundation in computer science, from algorithms to software engineering principles.",
+		"Beyond the classroom, I actively sharpen my practical skills and leadership abilities as a\n" +
+		"\t\t\t\tCore Team member of the Google Developer Student Clubs (GDSC), leading the Cloud Computing\n" +
+		"\t\t\t\tdivision. This role, along with my active participation in software development communities,\n" +
+		"\t\t\t\tallows me to collaborate, mentor, and stay current with industry trends.",
+		"To ensure my skills are always job-ready, I am committed to self-study on platforms like\n" +
+		"\t\t\t\tDicoding, Google Cloud Skills Boost, AWS Academy, and CodePolitan, completing specialized\n" +
+		"\t\t\t\tlearning paths in backend development, cloud architecture, and more.",
+	];
 </script>
 
 <div class="text-primary-dark flex flex-col lg:flex-row items-center justify-between px-4 md:px-10 lg:px-20 xl:px-52 pt-30 gap-5 lg:gap-32 pb-10">
+<!-- section icon	-->
+	<Motion
+		initial={{
+					scale: 0,
+				}}
+		animate={{
+					scale: 1,
+				}}
+		transition={{
+					type: 'spring',
+					stiffness: 400,
+					damping: 22,
+					delay: 1.6
+				}}
+		let:motion
+	>
+		<div use:motion class="relative flex items-baseline lg:items-start justify-center lg:order-1">
+			<div class="relative w-full sm:px-20 md:px-40 lg:px-0">
+				<CirclePattern />
+			</div>
+			<div class="absolute z-10 w-full sm:px-20 md:px-40 lg:px-0">
+				<Meet />
+			</div>
+		</div>
+	</Motion>
+
+	<!-- section content -->
 	<div class="flex flex-col lg:w-3/4 lg:order-2">
-		<div class="inline-flex items-center gap-3 mb-8">
-			<Education />
-			<h1 class="section-header-icon" data-text="Education">Education</h1>
+		<Motion
+			initial={{
+				opacity: 0,
+				x: 200,
+				scale: 0.8
+			}}
+			animate={{
+				opacity: 1,
+				x: 0,
+				scale: 1
+			}}
+			transition={{
+				duration: 0.8,
+				ease: [0.25, 0.46, 0.45, 0.94],
+				type: 'spring',
+				stiffness: 200,
+				damping: 20,
+				delay: 1.6,
+			}}
+			let:motion
+		>
+			<div use:motion class="inline-flex items-center gap-3 mb-8">
+				<Education />
+				<h1 class="section-header-icon" data-text="Education">Education</h1>
+			</div>
+		</Motion>
+		{#each contents as content, i (i)}
+			<Motion
+				initial={{
+				opacity: 0,
+				y: 200,
+			}}
+				animate={{
+				opacity: 1,
+				y: 0,
+			}}
+				transition={{
+				duration: 0.8,
+				ease: [0.25, 0.46, 0.45, 0.94],
+				type: 'spring',
+				stiffness: 200,
+				damping: 20,
+				delay: 1.6 + i * 0.2,
+			}}
+				let:motion
+			>
+				<p use:motion class="section-description mb-5">
+					{content}
+				</p>
+			</Motion>
+		{/each}
 		</div>
-		<p class="section-description mb-5">
-			I am currently pursuing my degree in Informatics Engineering at the Electronic Engineering
-			Polytechnic Institute of Surabaya (PENS/EEPIS), where I've gained a strong theoretical
-			foundation in computer science, from algorithms to software engineering principles.
-		</p>
-		<p class="section-description mb-5">
-			Beyond the classroom, I actively sharpen my practical skills and leadership abilities as a
-			Core Team member of the Google Developer Student Clubs (GDSC), leading the Cloud Computing
-			division. This role, along with my active participation in software development communities,
-			allows me to collaborate, mentor, and stay current with industry trends.
-		</p>
-		<p class="section-description">
-			To ensure my skills are always job-ready, I am committed to self-study on platforms like
-			Dicoding, Google Cloud Skills Boost, AWS Academy, and CodePolitan, completing specialized
-			learning paths in backend development, cloud architecture, and more.
-		</p>
-	</div>
-	<div class="relative flex items-baseline lg:items-start justify-center lg:order-1">
-		<div class="relative w-full sm:px-20 md:px-40 lg:px-0">
-			<CirclePattern />
-		</div>
-		<div class="absolute z-10 w-full sm:px-20 md:px-40 lg:px-0">
-			<Meet />
-		</div>
-	</div>
 </div>
--- src/lib/components/Intro.svelte
+++ src/lib/components/Intro.svelte
@@ -1,40 +1,123 @@
-<script>
+<script lang="ts">
 	import CirclePattern from '$lib/core/icons/CirclePattern.svelte';
 	import Self from '$lib/core/icons/Self.svelte';
 	import Story from '$lib/core/icons/Story.svelte';
+	import { Motion } from 'svelte-motion';
+
+	const contents: string[] = [
+		"Full-Stack Web Development: Architecting, building, and deploying robust web applications\n" +
+		"\t\t\t\tdesigned for performance and scale.",
+		"Intelligent Feature Integration: Leveraging foundational skills in Machine Learning and IoT\n" +
+		"\t\t\t\tto add data-driven insights and smart connectivity to projects.",
+		"Cloud-Native Deployment: Proficient in cloud fundamentals on AWS and GCP, ensuring every\n" +
+		"\t\t\t\tproject I build is ready for a real-world, scalable deployment from day one."
+	];
 </script>
 
-<div class="text-primary-dark flex flex-col lg:flex-row items-center justify-between px-4 md:px-10 lg:px-20 xl:px-52 pt-30 gap-5">
+<div
+	class="text-primary-dark flex flex-col items-center justify-between gap-5 px-4 pt-30 md:px-10 lg:flex-row lg:px-20 xl:px-52"
+>
 	<div class="flex flex-col">
-		<div class="inline-flex items-center gap-3 mb-8">
+		<!--	section content	-->
+		<Motion
+			initial={{
+				opacity: 0,
+				x: 200,
+				scale: 0.8
+			}}
+			animate={{
+				opacity: 1,
+				x: 0,
+				scale: 1
+			}}
+			transition={{
+				duration: 0.8,
+				ease: [0.25, 0.46, 0.45, 0.94],
+				type: 'spring',
+				stiffness: 200,
+				damping: 20,
+				delay: 1,
+			}}
+			let:motion
+		>
+			<div class="mb-8 inline-flex items-center gap-3" use:motion>
 			<Story />
 			<h1 class="section-header-icon" data-text="Quick Intro">Quick Intro</h1>
 		</div>
-		<p class="section-description xl:w-3/4">
-			From a 2020 lockdown curiosity to a full-stack engineer, my five-year journey has been one of
-			rapid, hands-on learning and a relentless drive to build effective solutions.
-		</p>
+		</Motion>
+		<Motion
+			initial={{
+				opacity: 0,
+				y: 300,
+			}}
+			animate={{
+				opacity: 1,
+				y: 0,
+			}}
+			transition={{
+				duration: 0.8,
+				ease: [0.25, 0.46, 0.45, 0.94],
+				type: 'spring',
+				stiffness: 200,
+				damping: 20,
+				delay: 1.2
+			}}
+			let:motion
+		>
+			<p class="section-description xl:w-3/4" use:motion>
+				From a 2020 lockdown curiosity to a full-stack engineer, my five-year journey has been one of
+				rapid, hands-on learning and a relentless drive to build effective solutions.
+			</p>
+		</Motion>
 		<ul class="list-disc pl-6 text-lg xl:w-3/4">
-			<li>
-				Full-Stack Web Development: Architecting, building, and deploying robust web applications
-				designed for performance and scale.
-			</li>
-			<li>
-				Intelligent Feature Integration: Leveraging foundational skills in Machine Learning and IoT
-				to add data-driven insights and smart connectivity to projects.
-			</li>
-			<li>
-				Cloud-Native Deployment: Proficient in cloud fundamentals on AWS and GCP, ensuring every
-				project I build is ready for a real-world, scalable deployment from day one.
-			</li>
+			{#each contents as content, i (i)}
+				<Motion
+					initial={{
+				opacity: 0,
+				y: 200,
+			}}
+					animate={{
+				opacity: 1,
+				y: 0,
+			}}
+					transition={{
+				duration: 0.8,
+				ease: [0.25, 0.46, 0.45, 0.94],
+				type: 'spring',
+				stiffness: 200,
+				damping: 20,
+				delay: 1.2 + i * 0.2,
+			}}
+					let:motion
+				>
+					<li use:motion>{content}</li>
+				</Motion>
+			{/each}
 		</ul>
+		<!--	section icon	-->
 	</div>
-	<div class="relative flex items-baseline xl:items-end justify-center">
-		<div class="absolute w-full sm:px-20 md:px-40 lg:px-0">
-			<CirclePattern />
+	<Motion
+		initial={{
+					scale: 0,
+				}}
+		animate={{
+					scale: 1,
+				}}
+		transition={{
+					type: 'spring',
+					stiffness: 400,
+					damping: 22,
+					delay: 1.3
+				}}
+		let:motion
+	>
+		<div use:motion class="relative flex items-baseline justify-center xl:items-end">
+			<div class="absolute w-full sm:px-20 md:px-40 lg:px-0">
+				<CirclePattern />
+			</div>
+			<div class="relative z-10 w-full sm:px-20 md:px-40 lg:px-0">
+				<Self />
+			</div>
 		</div>
-		<div class="relative z-10 w-full sm:px-20 md:px-40 lg:px-0">
-			<Self />
-		</div>
-	</div>
+	</Motion>
 </div>
